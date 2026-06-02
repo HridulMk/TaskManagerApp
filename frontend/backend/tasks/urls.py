bb984eae1b22f521ea483e236a7b5c0e528bf47e
HridulMk <137876163+HridulMk@users.noreply.github.com>
2026-06-02 18:19:03 +0530

    Delete frontend/backend directory

--- frontend/backend/tasks/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import TaskListCreateView, TaskDetailView, TaskStatsView
-
-urlpatterns = [
-    path('tasks/', TaskListCreateView.as_view(), name='task-list-create'),
-    path('tasks/<int:pk>/', TaskDetailView.as_view(), name='task-detail'),
-    path('tasks/stats/', TaskStatsView.as_view(), name='task-stats'),
-]
